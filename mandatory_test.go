869ff00334d08949e43ba2b6b1d5d516874db881
Pedro Salgado <(steenzout@users.noreply.github.com)>
2017-04-11 17:13:51 -0600

    .editorconfig: removed newline.

---
+++ mandatory_test.go
@@ -0,0 +1,16 @@
+package env_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// TestSuite test suite for the env package.
+type TestSuite struct {
+	suite.Suite
+}
+
+// TestPackage run handlers package test suites.
+func TestPackage(t *testing.T) {
+}
