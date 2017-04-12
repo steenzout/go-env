36e6dcb692fb6573c9be0ef9674593503b851576
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-11 18:13:59 -0600

    optional.go: added GetOptionalInt() and GetOptionalString().

--- optional.go
+++ optional.go
@@ -1,1 +1,31 @@
 package env
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetOptionalInt returns the string value of the environment variable if it exists and is an integer;
+// otherwise it will return the given default value.
+func GetOptionalInt(v string, d int) int {
+	value, found := os.LookupEnv(v)
+	if !found {
+		return d
+	}
+
+	ivalue, err := strconv.Atoi(value)
+	if err != nil {
+		return d
+	}
+	return ivalue
+}
+
+// GetOptionalString returns the string value of the environment variable if it exists;
+// otherwise it will return the given default value.
+func GetOptionalString(v string, d string) string {
+	value, found := os.LookupEnv(v)
+	if !found {
+		return d
+	}
+	return value
+}
--- optional_test.go
+++ optional_test.go
@@ -1,1 +1,33 @@
-package env
+package env_test
+
+import (
+	"github.com/steenzout/go-env"
+)
+
+const (
+	// EnvInt name of the environment variable that contains an integer value.
+	EnvInt = "ENV_INT"
+	// EnvUnknown inexistent environment variable.
+	EnvUnknown = "ENV_UNKNOWN"
+	// EnvStr name of the environment variable that contains a string value.
+	EnvStr = "ENV_STR"
+)
+
+// OptionalTestSuite test suite for functions in optional.go.
+type OptionalTestSuite struct {
+	PackageTestSuite
+}
+
+// Test check behavior of GetOptionalInt().
+func (s OptionalTestSuite) TestGetOptionalInt() {
+	s.Equal(0, env.GetOptionalInt(EnvUnknown, 0))
+	s.Equal(0, env.GetOptionalInt(EnvStr, 0))
+	s.Equal(1, env.GetOptionalInt(EnvInt, 0))
+}
+
+// Test check behavior of GetOptionalString().
+func (s OptionalTestSuite) TestGetOptionalString() {
+	s.Equal("default", env.GetOptionalString(EnvUnknown, "default"))
+	s.Equal("str", env.GetOptionalString(EnvStr, "default"))
+	s.Equal("1", env.GetOptionalString(EnvInt, "default"))
+}
