8f78d9151ab97455126cb2b36ad7c9074da760d3
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-11 18:14:18 -0600

    package_test.go: added package test suite.

--- package_test.go
+++ package_test.go
@@ -1,1 +1,28 @@
-package env
+package env_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type PackageTestSuite struct {
+	suite.Suite
+}
+
+func (s PackageTestSuite) SetupTest() {
+	os.Clearenv()
+	os.Setenv(EnvInt, "1")
+	os.Setenv(EnvStr, "str")
+}
+
+func (s PackageTestSuite) TearDownTest() {
+	os.Clearenv()
+}
+
+// TestPackage run all test suites included in the env package.
+func TestPackage(t *testing.T) {
+	suite.Run(t, new(MandatoryTestSuite))
+	suite.Run(t, new(OptionalTestSuite))
+}
