3fab8e0a26633f1cf673ced9cf76f5837180954f
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-11 18:13:38 -0600

    mandatory.go: implemented GetString() and GetInt().

--- mandatory.go
+++ mandatory.go
@@ -1,1 +1,38 @@
 package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	// ErrMandatoryVariable error message format for the case when an environment variable is missing.
+	ErrMandatoryVariable = "Environment variable %s is mandatory"
+	// ErrValueType error message format for the case when an environment variable has an unexpected type.
+	ErrValueType = "Environment variable %s is not of type %s"
+)
+
+// GetInt returns the int value of a mandatory environment variable.
+func GetInt(v string) int {
+	value, found := os.LookupEnv(v)
+	if !found {
+		panic(fmt.Sprintf(ErrMandatoryVariable, v))
+	}
+
+	ivalue, err := strconv.Atoi(value)
+	if err != nil {
+		panic(fmt.Sprintf(ErrValueType, v, "int"))
+	}
+	return ivalue
+}
+
+// GetString returns the string value of a mandatory environment variable.
+func GetString(v string) string {
+
+	value, found := os.LookupEnv(v)
+	if !found {
+		panic(fmt.Sprintf(ErrMandatoryVariable, v))
+	}
+	return value
+}
--- mandatory_test.go
+++ mandatory_test.go
@@ -1,16 +1,25 @@
 package env_test
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/suite"
+	"github.com/steenzout/go-env"
 )
 
-// TestSuite test suite for the env package.
-type TestSuite struct {
-	suite.Suite
+// MandatoryTestSuite test suite for functions in mandatory.go.
+type MandatoryTestSuite struct {
+	PackageTestSuite
 }
 
-// TestPackage run handlers package test suites.
-func TestPackage(t *testing.T) {
+// Test check behavior of GetInt().
+func (s MandatoryTestSuite) TestGetInt() {
+	s.Panics(func() { env.GetInt(EnvUnknown) })
+	s.Panics(func() { env.GetInt(EnvStr) })
+	s.Equal(1, env.GetInt(EnvInt))
+
+}
+
+// Test check behavior of GetString().
+func (s MandatoryTestSuite) TestGetString() {
+	s.Panics(func() { env.GetString(EnvUnknown) })
+	s.Equal("str", env.GetString(EnvStr))
+	s.Equal("1", env.GetString(EnvInt))
 }
