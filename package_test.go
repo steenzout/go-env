e24d167afdad6d9ea41c20bcb881b1b7152b94ab
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-12 09:34:41 -0600

    package_test.go: added CleanEnvTestSuite and run CleanEnvTestSuite + RedisTestSuite.

--- package_test.go
+++ package_test.go
@@ -7,22 +7,43 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// PackageTestSuite this package test suite.
 type PackageTestSuite struct {
 	suite.Suite
 }
 
+// SetupTest sets test environment variables.
 func (s PackageTestSuite) SetupTest() {
 	os.Clearenv()
 	os.Setenv(EnvInt, "1")
 	os.Setenv(EnvStr, "str")
 }
 
+// TearDownTest clears all environment variables.
 func (s PackageTestSuite) TearDownTest() {
 	os.Clearenv()
 }
 
 // TestPackage run all test suites included in the env package.
 func TestPackage(t *testing.T) {
+	// test generic functions
 	suite.Run(t, new(MandatoryTestSuite))
 	suite.Run(t, new(OptionalTestSuite))
+
+	// test app specific functions
+	suite.Run(t, new(RedisTestSuite))
+
+	// test defaults
+	suite.Run(t, new(ClearEnvSuite))
+}
+
+// ClearEnvSuite test suite with zero configuration set.
+// The purpose of this struct is to be extended with
+// more test functions.
+type ClearEnvSuite struct {
+	suite.Suite
+}
+
+func (s ClearEnvSuite) SetupTest() {
+	os.Clearenv()
 }
